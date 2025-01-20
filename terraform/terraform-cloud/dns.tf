5641a373512418069f0c398d04a0cfce06c67115
wanghongzhi <wanghongzhi@jiankongyouyi.com>
2025-01-20 14:45:55 +0800

    3fix

--- terraform/terraform-cloud/dns.tf
+++ terraform/terraform-cloud/dns.tf
@@ -4,7 +4,7 @@ resource "alicloud_alidns_record" "mfa_dns_record" {
   domain_name = "aliyun.18600113834.shop" # 替换为你的顶级域名
   rr          = "mfa"                      # 子域名部分
   type        = "A"                        # DNS记录类型，这里以A记录为例
-  value       = "10.8.1.25"               # A记录对应的IP地址，根据实际需求替换
+  value       = "10.8.1.35"               # A记录对应的IP地址，根据实际需求替换
   ttl         = 600                        # TTL值，可选，默认是600秒
 }
 
