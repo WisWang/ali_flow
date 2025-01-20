c0a0b4140226f362f8c12cbeec4af860f2f85dc8
wanghongzhi <wanghongzhi@jiankongyouyi.com>
2025-01-20 10:08:46 +0800

    wis-20250120

--- terraform/terraform-cloud/dns.tf
+++ terraform/terraform-cloud/dns.tf
@@ -4,7 +4,7 @@ resource "alicloud_dns_domain" "example_domain" {
 }
 
 resource "alicloud_dns_record" "example_a_record" {
-  domain_name = alicloud_dns_domain.example_domain.name
+  domain_id   = alicloud_dns_domain.example_domain.id
   type        = "A"
   host_record        = "mfa"
   value       = "192.168.1.100"
